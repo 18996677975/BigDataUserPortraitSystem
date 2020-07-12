96f9df84f9405304e3b0fbef34c06c9343435ffd
Chen Yu <58323844+18996677975@users.noreply.github.com>
2020-07-12 21:26:26 +0800

    Create trans.js

---
+++ static/js/trans.js
@@ -0,0 +1,72 @@
+function firstpage() {
+    var div1=  document.getElementById("first");
+    if(div1.style.display="block");
+    div1.style.display="block";
+    var div2=  document.getElementById("two");
+    if(div2.style.display="block");
+    div2.style.display="none";
+    var div3=  document.getElementById("three");
+    if(div3.style.display="block");
+    div3.style.display="none";
+    var div4=  document.getElementById("four");
+    if(div4.style.display="block");
+    div4.style.display="none";
+}
+function service(){
+   var div7=  document.getElementById("first");
+   if(div7.style.display="block");
+   div7.style.display="none";
+    var div2=  document.getElementById("two");
+    if(div2.style.display="block");
+    div2.style.display="block";
+    var div3=  document.getElementById("three");
+    if(div3.style.display="block");
+    div3.style.display="none";
+    var div4=  document.getElementById("four");
+    if(div4.style.display="block");
+    div4.style.display="none";
+}
+
+function aboutus() {
+    var div1=  document.getElementById("first");
+    if(div1.style.display="block");
+    div1.style.display="none";
+    var div2=  document.getElementById("two");
+    if(div2.style.display="block");
+    div2.style.display="none";
+    var div3=  document.getElementById("three");
+    if(div3.style.display="block");
+    div3.style.display="block";
+    var div4=  document.getElementById("four");
+    if(div4.style.display="none");
+    div4.style.display="none";
+}
+function connectus() {
+    var div1=  document.getElementById("first");
+    if(div1.style.display="block");
+    div1.style.display="none";
+    var div2=  document.getElementById("two");
+    if(div2.style.display="block");
+    div2.style.display="none";
+    var div3=  document.getElementById("three");
+    if(div3.style.display="none");
+    div3.style.display="none";
+    var div4=  document.getElementById("four");
+    if(div4.style.display="block");
+    div4.style.display="block";
+}
+//产品介绍
+function introduce() {
+    var m=document.getElementById("problem");
+    if(m.style.display="block")
+        m.style.display="none";
+    var n=document.getElementById("hide")
+    if(n.style.display="none");
+    n.style.display="block";
+}
+function out() {
+    document.getElementById("out").style.display = "block";
+}
+function sr() {
+    document.getElementById("out").style.display = "none";
+}
